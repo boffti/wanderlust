0b63e7899b0664eabf125a17eeb0f741d4d4df0d
Aneesh Melkot <boffti@gmail.com>
2021-04-24 18:23:52 -0500

    Minor Refactor and Beautify

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
@@ -10,12 +9,9 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\BusinessController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SearchController;
-use App\Mail\QueryEmail;
-use Illuminate\Http\Request;
-use App\Models\Business;
-use App\Models\Query;
+use App\Http\Controllers\QueryController;
+use App\Http\Controllers\ChatController;
 use App\Models\Category;
-use App\Models\Chat;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +24,7 @@ use App\Models\Chat;
 |
 */
 
+// Static View Routes
 Route::view('/about', 'main_site/about');
 Route::view('/immigrant-services', 'main_site/immigrant_services');
 Route::view('/visitor-services', 'main_site/visitor_service');
@@ -35,44 +32,48 @@ Route::view('/contact', 'main_site/contact')->name('contact');
 Route::view('/login', 'login/login');
 Route::view('/signup', 'login/signup');
 
+// Home Route
 Route::get('/', [HomeController::class, 'home'])->name('home');
 
+// Login/Register Routes
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/signup-handler', [LoginController::class, 'signupHandler']);
 Route::post('/register-user', [LoginController::class, 'registerUser']);
+Route::get('/logout', [LoginController::class, 'logout']);
 
-Route::post('/change-loc', [HomeController::class, 'changeLoc']);
+// Home Page Misc Routes
 Route::get('/more-tips', [HomeController::class, 'moreTips'])->name('all-tips');
 Route::get('/more-posts', [HomeController::class, 'morePosts'])->name('all-posts');
 
+// Search Route
 Route::post('/search', [SearchController::class, 'search']);
 
-Route::get('/logout', [LoginController::class, 'logout']);
+// Change Location Route
+Route::post('/change-loc', [HomeController::class, 'changeLoc']);
 
-Route::get('/profile', [UserController::class, 'getProfile'])->name('profile');
-Route::post('/profile/edit/{user_id}', [UserController::class, 'editProfile']);
-
-Route::post('/post', [PostController::class, 'addPost']);
-Route::get('/post/delete/{post_id}/{loc}', [PostController::class, 'deletePost']);
-
-Route::post('/tip', [TipController::class, 'addTip']);
-Route::get('/tip/delete/{tip_id}/{loc}', [TipController::class, 'deleteTip']);
-
+// User Routes
 Route::get('/user/{user_id}', [UserController::class, 'getUser']);
 Route::post('/dp', [UserController::class, 'updateDP']);
 Route::post('/photo', [UserController::class, 'addPhoto']);
 Route::post('/video', [UserController::class, 'addVideo']);
+Route::get('/profile', [UserController::class, 'getProfile'])->name('profile');
+Route::post('/profile/edit/{user_id}', [UserController::class, 'editProfile']);
 
+// Business Routes
 Route::get('/business/{business_id}', [BusinessController::class, 'getBusiness']);
 Route::post('/business/photo/{business_id}', [BusinessController::class, 'addBusinessPhoto']);
 Route::get('/business/tips/{business_id}', [BusinessController::class, 'getBusinessTips'])->name('business-tips');
 Route::post('business/tips/{business_id}', [BusinessController::class, 'addBusinessTip']);
 Route::get('/business/reviews/{business_id}', [BusinessController::class, 'getBusinessReviews'])->name('business-reviews');
 Route::post('business/reviews/{business_id}', [BusinessController::class, 'addBusinessReview']);
+Route::post('/business/{loc}', [AdminController::class, 'addBusiness']);
+Route::get('/business/delete/{business_id}/{loc}', [AdminController::class, 'deleteBusiness']);
 
+// Admin Routes
 Route::get('/country-admin', [AdminController::class, 'countryAdmin'])->name('country-admin');
 Route::get('/super-admin', [AdminController::class, 'superAdmin'])->name('super-admin');
 
+// Misc CRUD Routes
 Route::post('/continent', [AdminController::class, 'addContinent']);
 Route::get('/continent/delete/{continent_id}', [AdminController::class, 'deleteContinent']);
 
@@ -85,57 +86,25 @@ Route::get('/city/delete/{city_id}/{loc}', [AdminController::class, 'deleteCity'
 Route::post('/admin/add', [AdminController::class, 'addAdmin']);
 Route::get('/admin/delete/{id}', [AdminController::class, 'deleteAdmin']);
 
-Route::post('/business/{loc}', [AdminController::class, 'addBusiness']);
-Route::get('/business/delete/{business_id}/{loc}', [AdminController::class, 'deleteBusiness']);
+Route::post('/post', [PostController::class, 'addPost']);
+Route::get('/post/delete/{post_id}/{loc}', [PostController::class, 'deletePost']);
 
-Route::post('/query', function(Request $request) {
-    $c = new Query;
-    $c -> first_name = $request->get('firstName');
-    $c -> last_name = $request->get('lastName');
-    $c -> country = $request->get('country');
-    $c -> email = $request->get('email');
-    $c -> phone = $request->get('phone');
-    $c -> type = $request->get('userType');
-    $c -> query = $request->get('query');
-    Mail::to($request->get('email'))->send(new QueryEmail());
-    $c -> save();
+Route::post('/tip', [TipController::class, 'addTip']);
+Route::get('/tip/delete/{tip_id}/{loc}', [TipController::class, 'deleteTip']);
 
-    return redirect()->route('contact');
-});
-
+// Query Routes
+Route::post('/query', [QueryController::class, 'sendQuery']);
 Route::get('/query/delete/{queryid}', [AdminController::class, 'deleteQuery']);
 Route::get('/user/delete/{user_id}', [AdminController::class, 'deleteUser']);
 
+// Chat Routes
+Route::get('/chat', [ChatController::class, 'getChat']);
+Route::get('/send-chat/{msg}', [ChatController::class, 'sendChat']);
+
+// Category Route
 Route::get('/category', function() {
     return Category::all();
 });
 
-Route::get('/chat', function() {
-    $chats = Chat::with('user')
-    ->orderBy('created_at', 'ASC')
-    ->take(20)
-    ->get();
-    return view('user/chat')
-    -> with('chats', $chats);
-});
-
-Route::get('/send-chat/{msg}', function(Request $request, $msg) {
-    $c = new Chat;
-    $c -> user_id = session('user')['user_id'];
-    $c -> message = $msg;
-    $c->save();
-    return session('user');
-});
-
-Route::get('/test', function() {
-    $results = Business::where('city_id', 1)
-        ->with(['city', 'category'])
-        -> get();
-        return $results;
-});
-
-Route::get('/session', function() {
-    return session('admin');
-});
-
+// City Resource
 Route::resource('city', '\App\Http\Controllers\CityController');

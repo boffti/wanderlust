65e646e3cfa999b3aa6050116f3ef9e10040d3b0
Aneesh Melkot <boffti@gmail.com>
2021-05-25 18:47:43 -0500

    About page localization

--- resources/views/main_site/about.blade.php
+++ resources/views/main_site/about.blade.php
@@ -17,8 +17,8 @@ ID: 1001718335
         <div class="container">
             <div class="flex-left" style="justify-content: space-between; align-items:center;">
                 <div class="flex-column">
-                    <h2 class="display-4">Welcome to Diaspora</h2>
-                    <h4 class="lead text-muted">Your space, your voice, your home.</h4>
+                    <h2 class="display-4">{{ __("Welcome to Diaspora") }}</h2>
+                    <h4 class="lead text-muted">{{ __("Your space, your voice, your home.") }}</h4>
                 </div>
                 <img src="{{ URL::asset('img/route.png') }}" alt="hero-img" style="padding: 50px 0px; width:400px;" />
             </div>
@@ -31,10 +31,9 @@ ID: 1001718335
         <div class="container py-5">
             <div class="row align-items-center mb-5">
                 <div class="col-lg-6 order-2 order-lg-1"><i class="fas fa-map-marker-alt fa-2x mb-3 text-success"></i>
-                    <h4 class="font-weight-light">Find places around you.</h4>
-                    <p class="font-italic text-muted mb-4">Moved to a new city? Feel at home, find things to do around
-                        you.</p>
-                    <a href="/"> <button class="btn btn-success text-white">Start Now</button>
+                    <h4 class="font-weight-light">{{ __("Find places around you.") }}</h4>
+                    <p class="font-italic text-muted mb-4">{{ __("Moved to a new city? Feel at home, find things to do around you.") }}</p>
+                    <a href="/"> <button class="btn btn-success text-white">{{ __("Start Now") }}</button>
                     </a>
                 </div>
                 <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="{{ URL::asset('img/world.png') }}" alt=""
@@ -45,20 +44,17 @@ ID: 1001718335
                 <div class="col-lg-5 px-5 mx-auto"><img src="{{ URL::asset('img/passport.png') }}" alt=""
                         class="img-fluid mb-4 mb-lg-0"></div>
                 <div class="col-lg-6"> <i class="fas fa-comments fa-2x mb-3 text-success"></i>
-                    <h4 class="font-weight-light">Meet new people</h4>
-                    <p class="font-italic text-muted mb-4">Diaspora is your passport to find some really cool place
-                        and interact with poeple.
-                    </p>
-                    <a href=""> <button class="btn btn-success text-white">Chat Now</button>
+                    <h4 class="font-weight-light">{{ __("Meet new people") }}</h4>
+                    <p class="font-italic text-muted mb-4">{{ __("Diaspora is your passport to find some really cool place and interact with poeple.") }}</p>
+                    <a href=""> <button class="btn btn-success text-white">{{ __("Chat Now") }}</button>
                     </a>
                 </div>
             </div>
             <div class="row align-items-center mb-5">
                 <div class="col-lg-6 order-2 order-lg-1">
                     <i class="fas fa-list fa-2x mb-3 text-success"></i>
-                    <h4 class="font-weight-light">Make a dope profile</h4>
-                    <p class="font-italic text-muted mb-4">Create a profile worth showing off. Add photos and videos of
-                        your exploits.</p>
+                    <h4 class="font-weight-light"></h4>
+                    <p class="font-italic text-muted mb-4">{{ __("Create a profile worth showing off. Add photos and videos of your exploits.") }}</p>
                 </div>
                 <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="{{ URL::asset('img/profile.png') }}" alt=""
                         class="img-fluid mb-4 mb-lg-0"></div>
@@ -70,7 +66,7 @@ ID: 1001718335
 
         <section>
             <div class="container">
-                <p style=font-size:1.5rem;>Made with <i class="fas fa-heart" style="color: var(--accent-color);"></i> by
+                <p style=font-size:1.5rem;>{{ __("Made with") }} <i class="fas fa-heart" style="color: var(--accent-color);"></i> {{ __("by") }}
                 </p>
                 <div class="flex-left" style="justify-content:space-between;">
                     <div class="card flex-center">
